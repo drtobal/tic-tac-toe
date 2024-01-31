f472e429745c2bd1ea3778ce406a0d6edfaadad5
Cristóbal Díaz Álvarez <diazalvarezcristobal@gmail.com>
2024-01-31 10:06:39 -0300

    docs: commented constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,10 @@
+/** default board size, tic tac toe is a 3x3 board */
 export const BOARD_SIZE = 3;
 
+/** start player is X */
 export const START_PLAYER = 0;
 
+/** values for turns */
 export enum TurnList {
     x = 0,
     o = 1,
